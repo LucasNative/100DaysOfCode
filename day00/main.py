a9e32b47620de5fec6619753b1d910dae9cc06b6
lucasnative <santos.lucasdanielsp@gmail.com>
2021-10-11 17:29:23 -0300

    add day00

---
+++ day00/main.py
@@ -0,0 +1,11 @@
+###############################
+##       100DaysOfCode       ##
+##       Day 0               ##
+###############################
+
+# Band name generator
+
+print("\nWelcome to band name generator.\n")
+city = input("What's name of the city you grew up in? ")
+pet_name = input("What's your pet's name? ")
+print(f"Your band name could be {city} {pet_name}\n")
